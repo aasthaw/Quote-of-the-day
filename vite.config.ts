54706e185e2ee27d9714445bc2a7c51061a16aaf
Aastha <aasthawalia32@gmail.com>
2025-08-20 08:51:50 -0400

    chnages

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Must match your repo name exactly
-const repoName = 'Quote-of-the-day'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: `/${repoName}/`,
   plugins: [react()],
+  base: "/Quote-of-the-day/", // 👈 must match your repo name
   server: {
     proxy: {
-      '/zen': {
-        target: 'https://zenquotes.io',
+      "/zen": {
+        target: "https://zenquotes.io",
         changeOrigin: true,
-        secure: true,
-        rewrite: (p) => p.replace(/^\/zen/, ''),
+        rewrite: (path) => path.replace(/^\/zen/, ""),
       },
     },
   },
-})
+});
