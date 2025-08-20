7dc892783789087366a5c042e828f3efb83ba90e
Aastha <aasthawalia32@gmail.com>
2025-08-20 07:01:30 -0400

    fix: set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/quote-of-the-day/',   
+  base: '/Quote-of-the-day/',   
   server: {
     proxy: {
       '/zen': {
